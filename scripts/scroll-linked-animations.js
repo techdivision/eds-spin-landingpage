58483e5b75e48e3a741a45fcd1a0bcf836e678f7
voggenauerm <m.voggenauer@techdivision.com>
2023-09-04 10:17:23 +0200

    SPIN-92 // Make the passive event listener option backwards compatible

--- scripts/scroll-linked-animations.js
+++ scripts/scroll-linked-animations.js
@@ -41,8 +41,23 @@ export default function registerScrollLinkedAnimation(element) {
   calculateScrollPositions();
 }
 
+// Test via a getter in the options object to see if the passive property is accessed
+let supportsPassive = false;
+try {
+  const opts = Object.defineProperty({}, 'passive', {
+    // eslint-disable-next-line getter-return
+    get() {
+      supportsPassive = true;
+    },
+  });
+  window.addEventListener('testPassive', null, opts);
+  window.removeEventListener('testPassive', null, opts);
+} catch (e) {
+  // swallow
+}
+
 window.addEventListener(
   'scroll',
   calculateScrollPositions,
-  { passive: true },
+  supportsPassive ? { passive: true } : false,
 );
