27046b7f03d6ea291da26d91cff4cb8dd1b4aaa9
voggenauerm <m.voggenauer@techdivision.com>
2023-09-04 09:17:10 +0200

    SPIN-92 // Change planet class name

--- blocks/scroll/scroll.js
+++ blocks/scroll/scroll.js
@@ -2,7 +2,7 @@ import registerScrollLinkedAnimation from "/scripts/scroll-linked-animations.js"
 
 export default function decorate(block) {
   block.innerHTML = `
-      <div class="planet"></div>
+      <div class="my-planet"></div>
       <h1>LET'S SPIN YOUR WORLD</h1>
       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
       <div class="word-cloud-wrapper">
