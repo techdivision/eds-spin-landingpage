e86ee32cc63aa14d93d8398c87eaf0ed20cd0d92
voggenauerm <m.voggenauer@techdivision.com>
2023-09-04 15:43:25 +0200

    SPIN-92 // Add more animations for performance testing

--- blocks/scroll/scroll.js
+++ blocks/scroll/scroll.js
@@ -42,6 +42,78 @@ export default function decorate(block) {
             <span class="word">Vermarktung</span>
           </div>
         </div>
+        <div class="word-cloud-wrapper bottom-top">
+          <div class="word-cloud">
+            <span class="word">Analyse</span>
+            <span class="word">Shop</span>
+            <span class="word">E-Commerce</span>
+            <span class="word">Kreation</span>
+            <span class="word">Vermarktung</span>
+          </div>
+        </div>
+        <div class="word-cloud-wrapper bottom-top">
+          <div class="word-cloud">
+            <span class="word">Analyse</span>
+            <span class="word">Shop</span>
+            <span class="word">E-Commerce</span>
+            <span class="word">Kreation</span>
+            <span class="word">Vermarktung</span>
+          </div>
+        </div>
+        <div class="word-cloud-wrapper bottom-top">
+          <div class="word-cloud">
+            <span class="word">Analyse</span>
+            <span class="word">Shop</span>
+            <span class="word">E-Commerce</span>
+            <span class="word">Kreation</span>
+            <span class="word">Vermarktung</span>
+          </div>
+        </div>
+        <div class="word-cloud-wrapper bottom-top">
+          <div class="word-cloud">
+            <span class="word">Analyse</span>
+            <span class="word">Shop</span>
+            <span class="word">E-Commerce</span>
+            <span class="word">Kreation</span>
+            <span class="word">Vermarktung</span>
+          </div>
+        </div>
+                <div class="word-cloud-wrapper bottom-top">
+          <div class="word-cloud">
+            <span class="word">Analyse</span>
+            <span class="word">Shop</span>
+            <span class="word">E-Commerce</span>
+            <span class="word">Kreation</span>
+            <span class="word">Vermarktung</span>
+          </div>
+        </div>
+        <div class="word-cloud-wrapper bottom-top">
+          <div class="word-cloud">
+            <span class="word">Analyse</span>
+            <span class="word">Shop</span>
+            <span class="word">E-Commerce</span>
+            <span class="word">Kreation</span>
+            <span class="word">Vermarktung</span>
+          </div>
+        </div>
+        <div class="word-cloud-wrapper bottom-top">
+          <div class="word-cloud">
+            <span class="word">Analyse</span>
+            <span class="word">Shop</span>
+            <span class="word">E-Commerce</span>
+            <span class="word">Kreation</span>
+            <span class="word">Vermarktung</span>
+          </div>
+        </div>
+        <div class="word-cloud-wrapper bottom-top">
+          <div class="word-cloud">
+            <span class="word">Analyse</span>
+            <span class="word">Shop</span>
+            <span class="word">E-Commerce</span>
+            <span class="word">Kreation</span>
+            <span class="word">Vermarktung</span>
+          </div>
+        </div>
       </div>
       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
 `;
