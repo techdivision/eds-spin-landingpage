bb0438624d1b66dd30cb47f7dbc047d924ef1038
voggenauerm <m.voggenauer@techdivision.com>
2023-09-05 11:19:04 +0200

    SPIN-92 // Add throttling and animation frames to improve performance (maybe)

--- scripts/scroll-linked-animations.js
+++ scripts/scroll-linked-animations.js
@@ -1,4 +1,46 @@
 const scrollLinkedElements = [];
+const scrollPositions = {};
+let animationStart;
+
+function setScrollPositionsWithAnimation(timeStamp) {
+  if (animationStart === undefined) {
+    animationStart = timeStamp;
+  }
+
+  let done = true;
+
+  Object.values(scrollPositions).forEach((elementData) => {
+    if (elementData.currentScrollPosition === elementData.targetScrollPosition) {
+      return;
+    }
+    let scrollPosition;
+    if (Number.isNaN(elementData.currentScrollPosition) || elementData.currentScrollPosition === '') {
+      scrollPosition = elementData.targetScrollPosition;
+    } else if (elementData.currentScrollPosition < elementData.targetScrollPosition) {
+      scrollPosition = Math.min(
+        elementData.currentScrollPosition + elementData.scrollPositionDelta,
+        elementData.targetScrollPosition,
+      );
+    } else {
+      scrollPosition = Math.max(
+        elementData.currentScrollPosition + elementData.scrollPositionDelta,
+        elementData.targetScrollPosition,
+      );
+    }
+
+    if (!Number.isNaN(scrollPosition)) {
+      elementData.element.style.setProperty('--scroll', scrollPosition);
+      elementData.currentScrollPosition = scrollPosition;
+      if (scrollPosition !== elementData.targetScrollPosition) {
+        done = false;
+      }
+    }
+  });
+
+  if (!done) {
+    window.requestAnimationFrame(setScrollPositionsWithAnimation);
+  }
+}
 
 function calculateScrollPositions() {
   scrollLinkedElements.forEach((elementData) => {
@@ -11,10 +53,29 @@ function calculateScrollPositions() {
       ),
       1,
     );
-    elementData.element.style.setProperty('--scroll', scrollPercent);
+    // elementData.element.style.setProperty('--scroll', scrollPercent);
+    scrollPositions[elementData.element.classList] = {
+      element: elementData.element,
+      targetScrollPosition: scrollPercent,
+      currentScrollPosition: parseFloat(elementData.element.style.getPropertyValue('--scroll')),
+      scrollPositionDelta: (scrollPercent - parseFloat(elementData.element.style.getPropertyValue('--scroll'))) / 300,
+    };
+    window.requestAnimationFrame(setScrollPositionsWithAnimation);
     elementData.element.style.setProperty('--container-height', `${elementRect.height}px`);
     elementData.element.style.setProperty('--container-width', `${elementRect.width}px`);
   });
+}
+
+let isThrottled = false;
+function scheduleCalculation() {
+  if (isThrottled) return;
+  isThrottled = true;
+
+  calculateScrollPositions();
+  setTimeout(() => {
+    isThrottled = false;
+    calculateScrollPositions();
+  }, 300);
 }
 
 /**
@@ -26,7 +87,7 @@ function calculateScrollPositions() {
 export default function registerScrollLinkedAnimation(element, scrollStartPosition = 'top', scrollEndPosition = 'bottom') {
   const scrollPositions = ['top', 'bottom'];
   if (!scrollPositions.includes(scrollStartPosition)
-      || !scrollPositions.includes(scrollEndPosition)) {
+        || !scrollPositions.includes(scrollEndPosition)) {
     return;
   }
   let scrollStartOffset = 0;
@@ -65,6 +126,6 @@ try {
 
 window.addEventListener(
   'scroll',
-  calculateScrollPositions,
+  scheduleCalculation,
   supportsPassive ? { passive: true } : false,
 );
