00691b6f12e0d6118021be51a853342864d69b16
Markus Biberger <m.biberger@techdivision.com>
2023-09-06 16:53:35 +0200

    [SPIN-106] Update animation for stras

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
-import registerScrollLinkedAnimation from './scroll-linked-animations.js';
 import addStarsInUniverse from './content-stars.js';
+import registerScrollLinkedVariable, { VIEWPORT_TOP, VIEWPORT_BOTTOM } from './scroll-linked-variable.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 // add more delayed functionality here
-registerScrollLinkedAnimation(document.body);
+registerScrollLinkedVariable(document.body, VIEWPORT_TOP, VIEWPORT_BOTTOM);
 addStarsInUniverse();
