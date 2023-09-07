e6ab2d32b6fb7b66a379d4701a5690fdb6b9772f
voggenauerm <m.voggenauer@techdivision.com>
2023-09-07 13:51:24 +0200

    SPIN-92 // Add debounce function to reduce concurrent function calls on resize

---
+++ scripts/utilities.js
@@ -0,0 +1,26 @@
+/**
+ * Clamp a number between a min and max value.
+ *
+ * @param {number} value
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+export function clamp(value, min, max) {
+  return Math.min(Math.max(value, min), max);
+}
+
+/**
+ * If a function is called multiple times within the given timeout only the last call will be executed
+ *
+ * @param func
+ * @param timeout
+ * @returns {(function(...[*]): void)|*}
+ */
+export function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
