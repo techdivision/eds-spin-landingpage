c5ba97af49be86442d3bd8b05edf1f96826dacba
voggenauerm <m.voggenauer@techdivision.com>
2023-09-15 09:50:37 +0200

    SPIN-96 // Add scroll highlight logic

--- blocks/static-scroll-columns/static-scroll-columns.js
+++ blocks/static-scroll-columns/static-scroll-columns.js
@@ -26,9 +26,41 @@ export default function decorate(block) {
     rightColumn.classList.add('static-scroll-column-pictures');
   }
 
+  const maxNumberOfElements = Math.max(firstRowElements.length, secondRowElements.length);
+  const elementHeight = 50;
+  const staticScrollColumnsHeight = (maxNumberOfElements + 2) * elementHeight;
+  block.style.setProperty('--height-static-scroll-columns', `${staticScrollColumnsHeight}vh`);
+  block.style.setProperty('--height-static-scroll-columns-progress-section', `${elementHeight}vh`);
+
+  const scrollProgressSections = [];
+  const scrollProgress = document.createElement('div');
+  scrollProgress.classList.add('static-scroll-columns-progress');
+  for (let i = 0; i < maxNumberOfElements; i += 1) {
+    const scrollProgressSection = document.createElement('div');
+    scrollProgressSection.classList.add('static-scroll-columns-progress-section');
+    scrollProgressSection.dataset.index = i.toString();
+    scrollProgress.append(scrollProgressSection);
+    scrollProgressSections.push(scrollProgressSection);
+  }
+
+  block.append(scrollProgress);
+
+  const ACTIVE_CLASS = 'active';
+
   const staticScrollIntersectionObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-
+      if (entry.isIntersecting) {
+        const activeElements = entry.target.closest('.static-scroll-columns').querySelectorAll(`.${ACTIVE_CLASS}`);
+        activeElements.forEach((element) => {
+          element.classList.remove(ACTIVE_CLASS);
+        });
+        firstRowElements.item(entry.target.dataset.index).classList.add(ACTIVE_CLASS);
+        secondRowElements.item(entry.target.dataset.index).classList.add(ACTIVE_CLASS);
+      }
     });
-  }, { rootMargin: '49% 0px' });
+  }, { rootMargin: '-49% 0px' });
+
+  scrollProgressSections.forEach((progress) => {
+    staticScrollIntersectionObserver.observe(progress);
+  });
 }
